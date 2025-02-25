da27a819d2a35cc23967b159e133b894a916c2cf
Dev Ambience <125862598+Devambience@users.noreply.github.com>
2025-02-25 22:40:58 +0530

    Update qrrefer.tsx

--- components/qrrefer.tsx
+++ components/qrrefer.tsx
@@ -36,21 +36,22 @@ function QRDialogContent() {
           <div className="flex justify-center gap-4">
             <a
               href="https://maps.app.goo.gl/aSoaRTUYxWVgVRfh8"
-              className="flex flex-col items-center border w-1/2 rounded-lg p-5"
+              className="flex flex-col items-center border w-1/2 rounded-2xl p-5"
             >
               <MapPin className="h-12 w-7" />
               <p>Navigate to maps</p>
             </a>
             <a
               href="tel:9911366659"
-              className="flex flex-col items-center border w-1/2 rounded-lg p-5"
+              className="flex flex-col items-center border w-1/2 rounded-2xl p-5"
             >
               <Phone className="h-12 w-7" />
               <p>Contact Us</p>
             </a>
           </div>
+          <br></br>
           <DrawerClose asChild>
-            <Button className="rounded bg-white text-black w-full" variant="outline">
+            <Button className="rounded bg-white text-black w-full py-5" variant="outline">
               Continue With App
             </Button>
           </DrawerClose>
