445ae204469697af758989b62622d9190087f9ad
Dev Ambience <125862598+Devambience@users.noreply.github.com>
2025-03-12 21:48:23 +0530

    Update bottom-navbar.tsx

--- components/bottom-navbar.tsx
+++ components/bottom-navbar.tsx
@@ -5,7 +5,7 @@ import { HomeIcon, ShareIcon, PaymentIcon, PhoneIcon, HeartIcon } from "@/compon
 const MobileBottomNavigation = () => {
     console.log("MobileBottomNavigation is rendering"); // Debugging
     return (
-      <div className="fixed bottom-0 left-0 right-0 backdrop-sepia-0 to-transparent backdrop-blur-xl bg-black/80 shadow-md flex justify-around items-center inset-shadow-md px-4 py-3 border rounded-t-3xl border-t block md:hidden">
+      <div className="fixed bottom-0 left-0 right-0 backdrop-sepia-0 to-transparent backdrop-blur-xl bg-black/80 shadow-md flex justify-around items-center inset-shadow-md px-4 py-1 border rounded-t-3xl border-t block md:hidden">
         <Link href="/" className="flex flex-col items-center text-white hover:text-white">
           <HomeIcon className="w-6 h-6" />
           <span className="text-xs">Home</span>
